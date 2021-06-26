b7f64e89fee4410e748b44dbbe104d44d713b1a9
Uros Teofanovic <uteofanovic4@gmail.com>
2021-06-26 17:47:00 +0200

    Added SearchBox component, some initial styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
+import { Container } from 'react-bootstrap';
+
+import SearchBox from './components/SearchBox';
+
 function App() {
-  return <div>Weather</div>;
+  return (
+    <Container fluid className="main-container">
+      <SearchBox />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,54 @@
+import './SearchBox.css';
+
+import React from 'react';
+import { Col, Dropdown, Row } from 'react-bootstrap';
+
+import searchIcon from '../assets/images/search.png';
+import weather from '../assets/images/weather.png';
+
+const SearchBox = () => {
+  return (
+    <Row className="p-4 justify-content-center">
+      <Col
+        xs={12}
+        md={10}
+        lg={7}
+        xl={5}
+        className="p-3 border rounded row align-items-center justify-content-center bg-light shadow"
+      >
+        <Col sm={1} className="d-none d-sm-flex justify-content-center">
+          <img src={weather} alt="" className="weather-icon" />
+        </Col>
+        <Col xs={3} className="d-flex justify-content-center">
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="info"
+              id="dropdown-basic"
+              size="lg"
+              className="country-dropdown"
+            >
+              Country
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+        <Col xs={8} className="position-relative">
+          <img src={searchIcon} alt="" className="search-icon" />
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            id="location"
+            placeholder="Please enter your location..."
+          />
+        </Col>
+      </Col>
+    </Row>
+  );
+};
+
+export default SearchBox;
